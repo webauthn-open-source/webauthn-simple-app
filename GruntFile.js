9fae1b26379a8541a6d321d1247a91d02e356299
Adam Powers <apowers@ato.ms>
2018-04-19 20:13:59 -0700

    narrowing down platform list

--- GruntFile.js
+++ GruntFile.js
@@ -5,25 +5,25 @@ module.exports = function(grunt) {
         browserName: "firefox",
         platform: "Windows 10"
     }, {
-        browserName: "firefox",
-        platform: "Windows 10",
-        version: "dev"
-    }, {
-        browserName: "firefox",
-        platform: "Windows 10",
-        version: "beta"
-    }, {
+    //     browserName: "firefox",
+    //     platform: "Windows 10",
+    //     version: "dev"
+    // }, {
+    //     browserName: "firefox",
+    //     platform: "Windows 10",
+    //     version: "beta"
+    // }, {
         browserName: "chrome",
         platform: "Windows 10"
     }, {
-        browserName: "chrome",
-        platform: "Windows 10",
-        version: "dev"
-    }, {
-        browserName: "chrome",
-        platform: "Windows 10",
-        version: "beta"
-    }, {
+    //     browserName: "chrome",
+    //     platform: "Windows 10",
+    //     version: "dev"
+    // }, {
+    //     browserName: "chrome",
+    //     platform: "Windows 10",
+    //     version: "beta"
+    // }, {
         browserName: "MicrosoftEdge",
         platform: "Windows 10"
     }, {
@@ -42,30 +42,30 @@ module.exports = function(grunt) {
         browserName: "chrome",
         platform: "macOS 10.12"
     }, {
-        browserName: "chrome",
-        platform: "macOS 10.12",
-        version: "dev"
-    }, {
-        browserName: "chrome",
-        platform: "macOS 10.12",
-        version: "beta"
-    }, {
+    //     browserName: "chrome",
+    //     platform: "macOS 10.12",
+    //     version: "dev"
+    // }, {
+    //     browserName: "chrome",
+    //     platform: "macOS 10.12",
+    //     version: "beta"
+    // }, {
         browserName: "firefox",
         platform: "macOS 10.12"
     }, {
-        // browserName: "firefox",
-        // platform: "macOS 10.12",
-        // version: "dev"
-    }, {
-        browserName: "firefox",
-        platform: "macOS 10.12",
-        version: "beta"
-    }, {
+    //     browserName: "firefox",
+    //     platform: "macOS 10.12",
+    //     version: "dev"
+    // }, {
+    //     browserName: "firefox",
+    //     platform: "macOS 10.12",
+    //     version: "beta"
+    // }, {
         browserName: "safari",
         platform: "macOS 10.12",
-    }, {
-        browserName: "chrome",
-        platform: "android"
+    // }, {
+    //     browserName: "chrome",
+    //     platform: "android"
     }];
 
     grunt.initConfig({
